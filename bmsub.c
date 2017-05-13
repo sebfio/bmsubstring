c863d3cca8f3896b15726d8a788623ff5aecbe03
Sebastian Fiorini <sebf465@gmail.com>
2017-05-12 22:53:37 -0400

    Init body

---
+++ bmsub.c
@@ -0,0 +1,9 @@
+/*
+ *	Smart substring search, basing this off of the Boyer-Moore substring
+ *	search. Found this from the Algos textbook fourth edition and I thought
+ *	it was cool.
+ */
+
+int main(int argc, char ** args) {
+
+}
